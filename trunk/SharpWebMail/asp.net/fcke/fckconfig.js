c40982028c2ef62baaaaa8b0aa667052608315f2
anmar <anmar@4b8e5d3a-8b10-0410-9ae9-e01485a2957d>
2004-12-01 20:19:32 +0000

    no message

--- trunk/SharpWebMail/asp.net/fcke/fckconfig.js
+++ trunk/SharpWebMail/asp.net/fcke/fckconfig.js
@@ -11,15 +11,18 @@
  * File Name: fckconfig.js
  * 	Editor configuration settings.
  * 
- * Version:  2.0 Beta 2
- * Modified: 2004-08-30 23:27:01
+ * Version:  2.0 RC1
+ * Modified: 2004-11-29 18:38:00
  * 
  * File Authors:
  * 		Frederico Caldeira Knabben (fredck@fckeditor.net)
  */
 
 // Custom Configurations (leave blank to ignore)
-FCKConfig.CustomConfigurationsPath = '' ;	// '/fckeditor.config.js' ;
+FCKConfig.CustomConfigurationsPath = '' ;
+
+// Style File to be used in the editable area.
+FCKConfig.EditorAreaCSS = FCKConfig.BasePath + 'css/fck_editorarea.css' ;
 
 // Enables the debug window
 FCKConfig.Debug = false ;
@@ -27,9 +30,11 @@ FCKConfig.Debug = false ;
 // Set the path for the skin files to use.
 FCKConfig.SkinPath = FCKConfig.BasePath + 'skins/default/' ;
 
+FCKConfig.PluginsPath = FCKConfig.BasePath + 'plugins/' ;
+
 // Language settings
 FCKConfig.AutoDetectLanguage = true ;
-FCKConfig.DefaultLanguage    = "en" ;
+FCKConfig.DefaultLanguage    = 'en' ;
 
 // Enable XHTML support
 FCKConfig.EnableXHTML		= false ;
@@ -44,6 +49,8 @@ FCKConfig.StartupFocus = false ;
 // Cut and Paste options
 FCKConfig.ForcePasteAsPlainText	= false ;
 
+FCKConfig.ForceSimpleAmpersand = false ;
+
 // Link: Target Windows
 FCKConfig.LinkShowTargets = true ;
 FCKConfig.LinkTargets = '_blank;_parent;_self;_top' ;
@@ -55,19 +62,16 @@ FCKConfig.ToolbarCanCollapse	= true ;
 //##
 //## Toolbar Buttons Sets
 //##
-FCKConfig.ToolbarSets = new Object() ;
 FCKConfig.ToolbarSets["Default"] = [
 /*	['Source','-','Save','NewPage','Preview'],*/
-	['Cut','Copy','Paste','PasteText','PasteWord','-','Print'/*,'SpellCheck'*/],
-	['Undo','Redo',/*'-','Find','Replace',*/'-','SelectAll','RemoveFormat'],
+	['Cut','Copy','Paste','PasteText','PasteWord'/*,'-','Print'*/],
+	['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
 	['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript'],
 	['OrderedList','UnorderedList','-','Outdent','Indent'],
 	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
-	['Link','Unlink'/*,'Anchor'*/],
-	['Image','Table','Rule','SpecialChar'/*,'UniversalKey'*/,'Smiley'],
-	/*['Form','Checkbox','Radio','Input','Textarea','Select','Button','ImageButton','Hidden']*/
-	/*['ShowTableBorders','ShowDetails','-','Zoom'],*/
-	[/*'FontStyleAdv','-','FontStyle','-','FontFormat','-',*/'FontName','-','FontSize'],
+	['Link','Unlink'],
+	['Image','Table','Rule','SpecialChar','Smiley'],
+	[/*'Style','FontFormat',*/'FontName','FontSize'],
 	['TextColor','BGColor'],
 	['About']
 ] ;
@@ -78,12 +82,19 @@ FCKConfig.ToolbarSets["Basic"] = [
 // Font Colors
 FCKConfig.FontColors = '000000,993300,333300,003300,003366,000080,333399,333333,800000,FF6600,808000,808080,008080,0000FF,666699,808080,FF0000,FF9900,99CC00,339966,33CCCC,3366FF,800080,999999,FF00FF,FFCC00,FFFF00,00FF00,00FFFF,00CCFF,993366,C0C0C0,FF99CC,FFCC99,FFFF99,CCFFCC,CCFFFF,99CCFF,CC99FF,FFFFFF' ;
 
-// Font Names
-FCKConfig.FontNames = 'Arial;Comic Sans MS;Courier New;Tahoma;Times New Roman;Verdana' ;
+// Font Combos
+FCKConfig.FontNames		= 'Arial;Comic Sans MS;Courier New;Tahoma;Times New Roman;Verdana' ;
+FCKConfig.FontSizes		= '1/xx-small;2/x-small;3/small;4/medium;5/large;6/x-large;7/xx-large' ;
+FCKConfig.FontFormats	= 'p;pre;address;h1;h2;h3;h4;h5;h6' ;
+
+FCKConfig.StylesXmlPath	= '../fckstyles.xml' ;
 
 // Link Browsing
 FCKConfig.LinkBrowser = false ;
-FCKConfig.LinkBrowserURL = FCKConfig.BasePath + "filemanager/browser/default/browser.html?Connector=connectors/aspx/connector.aspx" ;
+FCKConfig.LinkBrowserURL = FCKConfig.BasePath + "filemanager/browser/default/browser.html?Connector=connectors/asp/connector.asp" ;
+//FCKConfig.LinkBrowserURL = FCKConfig.BasePath + "filemanager/browser/default/browser.html?Connector=connectors/asp/connector.asp&ServerPath=/CustomFiles/" ;
+//FCKConfig.LinkBrowserURL = FCKConfig.BasePath + "filemanager/browser/default/browser.html?Connector=connectors/aspx/connector.aspx" ;
+//FCKConfig.LinkBrowserURL = FCKConfig.BasePath + "filemanager/browser/default/browser.html?Connector=connectors/php/connector.php" ;
 FCKConfig.LinkBrowserWindowWidth	= screen.width * 0.7 ;	// 70%
 FCKConfig.LinkBrowserWindowHeight	= screen.height * 0.7 ;	// 70%
 
@@ -93,11 +104,13 @@ FCKConfig.LinkUploadURL = FCKConfig.BasePath + "filemanager/upload/aspx/upload.a
 FCKConfig.LinkUploadWindowWidth		= 300 ;
 FCKConfig.LinkUploadWindowHeight	= 150 ;
 FCKConfig.LinkUploadAllowedExtensions	= "*" ;		// * or empty for all
-FCKConfig.LinkUploadDeniedExtensions	= ".exe .asp .php .aspx .js .cfm .dll" ;	// empty for no one
+FCKConfig.LinkUploadDeniedExtensions	= ".exe .asp .php .aspx .js .cfm .dll" ;	// empty for none
 
 // Image Browsing
 FCKConfig.ImageBrowser = false ;
-FCKConfig.ImageBrowserURL = FCKConfig.BasePath + "filemanager/browser/default/browser.html?Type=Image&Connector=connectors/aspx/connector.aspx" ;
+FCKConfig.ImageBrowserURL = FCKConfig.BasePath + "filemanager/browser/default/browser.html?Type=Image&Connector=connectors/asp/connector.asp" ;
+//FCKConfig.ImageBrowserURL = FCKConfig.BasePath + "filemanager/browser/default/browser.html?Type=Image&Connector=connectors/aspx/connector.aspx" ;
+//FCKConfig.ImageBrowserURL = FCKConfig.BasePath + "filemanager/browser/default/browser.html?Type=Image&Connector=connectors/php/connector.php" ;
 FCKConfig.ImageBrowserWindowWidth  = screen.width * 0.7 ;	// 70% ;
 FCKConfig.ImageBrowserWindowHeight = screen.height * 0.7 ;	// 70% ;
 
@@ -107,8 +120,3 @@ FCKConfig.SmileyImages	= ["regular_smile.gif","sad_smile.gif","wink_smile.gif","
 FCKConfig.SmileyColumns = 8 ;
 FCKConfig.SmileyWindowWidth		= 320 ;
 FCKConfig.SmileyWindowHeight	= 240 ;
-
-//**
-// Load the custom configurations file
-if ( FCKConfig.CustomConfigurationsPath.length > 0 )
-	FCKScriptLoader.AddScript( FCKConfig.CustomConfigurationsPath ) ;
