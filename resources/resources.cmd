f46254d0bb9fd837b552d73be5619235eca92f5c
moemura <srakti@live.com>
2020-05-12 14:52:39 +0700

    resource to resx and adjust resources.cmd

--- resources/resources.cmd
+++ resources/resources.cmd
@@ -8,7 +8,7 @@ REM Build Satellite assemblies
 
 cd ..\resources\
 
-for /F "tokens=2 delims=." %%i IN ('dir /B SharpWebMail.*.resources') DO call :resource %%i .%%i
+for /F "tokens=2 delims=." %%i IN ('dir /B SharpWebMail.*.resx') DO call :resource %%i .%%i
 goto :end
 
 :resource
@@ -17,6 +17,7 @@ SET FINALTARGET=..\%TARGET%
 IF NOT EXIST %FINALTARGET% (
 	mkdir %FINALTARGET%
 )
+resgen SharpWebMail.%1.resx SharpWebMail.%1.resources
 al.exe /nologo /target:library /embed:SharpWebMail%2.resources,SharpWebMail.%1.resources /culture:%1 /out:%FINALTARGET%\SharpWebMail.resources.dll /template:%FINALTARGET%\..\SharpWebMail.dll
 
 :end
